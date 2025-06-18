00abc19699d5fb58e4504e1938bb821a31c40165
pauline1102 <hansen.pauline@yahoo.dk>
2025-06-18 19:36:39 +0200

    Update app

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Paulines Website</h1>
+      <h1>Paulines Website 1000</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
