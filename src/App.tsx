5d3dcaa298ebb888106a839c51101139aef682db
pauline1102 <hansen.pauline@yahoo.dk>
2025-06-18 19:51:16 +0200

    Din kodeændring uden dist

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Paulines Website 1000 prut prut prut</h1>
-      <p>jeg skriver lige en tekst, ikke?</p>
+      <p>NU virker det</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
